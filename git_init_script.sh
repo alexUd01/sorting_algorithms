c743b5cdf287156ac2bf4103403a72f6ee7f8b69
alexUd01 <alexanderikpeama@gmail.com>
2023-01-20 11:02:24 +0100

    fixed pat

--- git_init_script.sh
+++ git_init_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git remote add origin https://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@github.com/alexUd01/sorting_algorithms.git
+git remote add origin https://<PAT>@github.com/alexUd01/sorting_algorithms.git
 
 git add .
 git commit -m "project init"
