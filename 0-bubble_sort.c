b9937e6530a8980b01766f4a4c414ca2c5b78953
alexUd01 <alexanderikpeama@gmail.com>
2023-01-20 11:21:22 +0100

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,6 @@
 #include "sort.h"
 
 /**
- * check_if_sorted - a function that checks if an array of int is already
- * sorted.
- * @arr: the array of integers
- * @size: the size of the array
- * Return: returns 1 if sorted, 0 otherwise
- */
-int check_if_sorted(int *arr, size_t size)
-{
-	size_t i;
-
-	for (i = 0; i < size - 1; i++)
-	{
-		if (arr[i] > arr[i + 1])
-			return (0);
-	}
-	return (1);
-}
-
-/**
  * bubble_sort - a function that sorts an array of integers using bubble sort
  * algorithm.
  * @array: the array to be sorted
@@ -29,8 +10,6 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, temp;
 
-	if (check_if_sorted(array, size))
-		return;
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < (size - 1); j++)
