ee4256d20f26a91f8c3fad21a17d6ec7586225da
alexUd01 <alexanderikpeama@gmail.com>
2023-01-23 10:32:34 +0100

    optimized task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,16 +8,15 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, temp;
+	size_t i = 0, j, temp;
+	int swapped = 0;
 
-	if (size < 2)
+	if (size < 2 || array == NULL)
 		return;
 
-	if (array == NULL)
-		return;
+	do {
+		swapped = 0;
 
-	for (i = 0; i < size; i++)
-	{
 		for (j = 0; j < (size - (i + 1)); j++)
 		{
 			if (array[j] > array[j + 1])
@@ -26,7 +25,10 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
+
+				swapped = 1;
 			}
 		}
-	}
+
+	} while ((++i < size) && swapped);
 }
